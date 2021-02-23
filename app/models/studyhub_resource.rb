bd3bf95519b89aece68c2e4dbde358f0cfa4909e
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-02-23 23:12:59 +0100

    add more validations for StudyhubResource model

--- app/models/studyhub_resource.rb
+++ app/models/studyhub_resource.rb
@@ -1,8 +1,14 @@
 class StudyhubResource < ApplicationRecord
 
-  has_many :studyhub_resource_links, dependent: :destroy
-  has_one :study, through: :studyhub_resource_links, source: :resource, source_type: 'Study'
-  has_one :assay, through: :studyhub_resource_links, source: :resource, source_type: 'Assay'
+  has_one :studyhub_resource_link, dependent: :destroy
+  has_one :study, through: :studyhub_resource_link, source: :resource, source_type: 'study', dependent: :destroy
+  has_one :assay, through: :studyhub_resource_link, source: :resource, source_type: 'assay', dependent: :destroy
+
+  validates :resource_id, presence: { message:"Studyhub Resource ID is blank or invalid" }, uniqueness: { message:"Studyhub Resource ID exsits" }
+  validates :resource_type, presence: { message:"Studyhub Resource Type is blank or invalid" }
+
+  store_accessor :resource_json,:studySecondaryOutcomes,:studyAnalysisUnit,:acronyms
+
 
   # STUDY = 'study'.freeze
   # DOCUMENT = 'document'.freeze
