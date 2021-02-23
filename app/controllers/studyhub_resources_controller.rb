cf556a88c89a23bf71ead79b1f361d7b825fd201
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-02-23 23:11:46 +0100

    create a simple StudyhubResource Controller (studyhub_resources_controller.rb) with actions "index","show" and "create"

---
+++ app/controllers/studyhub_resources_controller.rb
@@ -0,0 +1,41 @@
+class StudyhubResourcesController < ApplicationController
+
+  def index
+    @studyhub_resources = StudyhubResource.all
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml
+      format.json {render json: @studyhub_resources}
+    end
+  end
+
+  def show
+    @studyhub_resource = StudyhubResource.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml
+      format.json {render json: @studyhub_resource}
+    end
+  end
+
+  def create
+
+    @resource = StudyhubResource.new(studyhub_resource_params)
+
+    if @resource.save
+      render json: @resource, status: :created, location: @resource
+    else
+      render json: @resource.errors, status: :unprocessable_entity
+    end
+
+  end
+
+
+
+  def studyhub_re source_params
+    params.require(:studyhub_resource).permit(:parent_id,:resource_id,:resource_type, {resource_json: {}})
+  end
+
+
+end
