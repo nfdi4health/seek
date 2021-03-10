f9b3f46b5e360cebda053d4d1d5ca581cd101093
michael <michael.lieser@h-its.org>
2021-03-10 16:58:06 +0100

    removed parent_id from .._resources_serializer.rb

--- app/serializers/studyhub_resource_serializer.rb
+++ app/serializers/studyhub_resource_serializer.rb
@@ -1,5 +1,5 @@
 class StudyhubResourceSerializer < BaseSerializer
-  attributes :parent_id, :resource_id, :resource_type, :resource_json
+  attributes :resource_id, :resource_type, :resource_json
   has_one :study
   has_one :assay
 end
