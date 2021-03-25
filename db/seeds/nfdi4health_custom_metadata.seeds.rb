7837da575c53416c984748bcc557c87995a6baee
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-03-25 22:31:32 +0100

    distinguish two CustomMetadataTypes for Study and Assay

--- db/seeds/nfdi4health_custom_metadata.seeds.rb
+++ db/seeds/nfdi4health_custom_metadata.seeds.rb
@@ -18,56 +18,115 @@ disable_authorization_checks do
                                                { label: 'Document' }]
   )
 
-  # NFDI4Health Studyhub Study Metadata
-  CustomMetadataType.where(title: 'NFDI4Health Studyhub Resource Metadata').first_or_create!(
-    title: 'NFDI4Health Studyhub Resource Metadata', supported_type: 'Study',
+  # NFDI4Health Studyhub Study Metadata (resource_type: study and substudy)
+  CustomMetadataType.where(title: 'NFDI4Health Studyhub Study Metadata').first_or_create!(
+    title: 'NFDI4Health Studyhub Study Metadata', supported_type: 'Study',
     custom_metadata_attributes: [
 
-      CustomMetadataAttribute.where(title: 'resource_id').first_or_create!(
+      CustomMetadataAttribute.where(title: 'resource_id').create!(
         title: 'resource_id', required: true, sample_attribute_type: int_type
       ),
 
-      CustomMetadataAttribute.where(title: 'resource_web_studyhub').first_or_create!(
+      CustomMetadataAttribute.where(title: 'resource_web_studyhub').create!(
         title: 'resource_web_studyhub', required: true, sample_attribute_type: string_type
       ),
 
-      CustomMetadataAttribute.where(title: 'resource_web_page').first_or_create!(
+      CustomMetadataAttribute.where(title: 'resource_web_page').create!(
         title: 'resource_web_page', required: true, sample_attribute_type: string_type
       ),
-      CustomMetadataAttribute.where(title: 'resource_type').first_or_create!(
+      CustomMetadataAttribute.where(title: 'resource_type').create!(
         title: 'resource_type', required: true, sample_attribute_type: cv_type, sample_controlled_vocab: resource_type_cv
       ),
 
-      CustomMetadataAttribute.where(title: 'resource_web_mica').first_or_create!(
+      CustomMetadataAttribute.where(title: 'resource_web_mica').create!(
         title: 'resource_web_mica', required: true, sample_attribute_type: string_type
       ),
 
-      CustomMetadataAttribute.where(title: 'acronym').first_or_create!(
+      CustomMetadataAttribute.where(title: 'acronym').create!(
         title: 'acronym', required: true, sample_attribute_type: string_type
       ),
 
-      CustomMetadataAttribute.where(title: 'study_type').first_or_create!(
+      CustomMetadataAttribute.where(title: 'study_type').create!(
         title: 'study_type', required: true, sample_attribute_type: string_type
       ),
 
 
-      CustomMetadataAttribute.where(title: 'study_start_date').first_or_create!(
+      CustomMetadataAttribute.where(title: 'study_start_date').create!(
         title: 'study_start_date', required: true, sample_attribute_type: date_type
       ),
 
-      CustomMetadataAttribute.where(title: 'study_end_date').first_or_create!(
+      CustomMetadataAttribute.where(title: 'study_end_date').create!(
         title: 'study_end_date', required: true, sample_attribute_type: date_type
       ),
 
 
-      CustomMetadataAttribute.where(title: 'study_status').first_or_create!(
+      CustomMetadataAttribute.where(title: 'study_status').create!(
         title: 'study_status', required: true, sample_attribute_type: string_type
       ),
 
-      CustomMetadataAttribute.where(title: 'study_country').first_or_create!(
+      CustomMetadataAttribute.where(title: 'study_country').create!(
         title: 'study_country', required: true, sample_attribute_type: string_type
       ),
-      CustomMetadataAttribute.where(title: 'study_eligibility').first_or_create!(
+
+      CustomMetadataAttribute.where(title: 'study_eligibility').create!(
+        title: 'study_eligibility', required: true, sample_attribute_type: string_type
+      )
+
+    ]
+  )
+
+
+  # NFDI4Health Studyhub Resource Metadata (resource_type: except study and substudy)
+  CustomMetadataType.where(title: 'NFDI4Health Studyhub Resource Metadata').first_or_create!(
+    title: 'NFDI4Health Studyhub Resource Metadata', supported_type: 'Assay',
+    custom_metadata_attributes: [
+
+      CustomMetadataAttribute.where(title: 'resource_id').create!(
+        title: 'resource_id', required: true, sample_attribute_type: int_type
+      ),
+
+      CustomMetadataAttribute.where(title: 'resource_web_studyhub').create!(
+        title: 'resource_web_studyhub', required: true, sample_attribute_type: string_type
+      ),
+
+      CustomMetadataAttribute.where(title: 'resource_web_page').create!(
+        title: 'resource_web_page', required: true, sample_attribute_type: string_type
+      ),
+      CustomMetadataAttribute.where(title: 'resource_type').create!(
+        title: 'resource_type', required: true, sample_attribute_type: cv_type, sample_controlled_vocab: resource_type_cv
+      ),
+
+      CustomMetadataAttribute.where(title: 'resource_web_mica').create!(
+        title: 'resource_web_mica', required: true, sample_attribute_type: string_type
+      ),
+
+      CustomMetadataAttribute.where(title: 'acronym').create!(
+        title: 'acronym', required: true, sample_attribute_type: string_type
+      ),
+
+      CustomMetadataAttribute.where(title: 'study_type').create!(
+        title: 'study_type', required: true, sample_attribute_type: string_type
+      ),
+
+
+      CustomMetadataAttribute.where(title: 'study_start_date').create!(
+        title: 'study_start_date', required: true, sample_attribute_type: date_type
+      ),
+
+      CustomMetadataAttribute.where(title: 'study_end_date').create!(
+        title: 'study_end_date', required: true, sample_attribute_type: date_type
+      ),
+
+
+      CustomMetadataAttribute.where(title: 'study_status').create!(
+        title: 'study_status', required: true, sample_attribute_type: string_type
+      ),
+
+      CustomMetadataAttribute.where(title: 'study_country').create!(
+        title: 'study_country', required: true, sample_attribute_type: string_type
+      ),
+
+      CustomMetadataAttribute.where(title: 'study_eligibility').create!(
         title: 'study_eligibility', required: true, sample_attribute_type: string_type
       )
     ]
