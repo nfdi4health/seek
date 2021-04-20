5829449128352a874fce64619c1a48805717a2db
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-04-20 17:47:02 +0200

    change seek url to studyhub web url "http://covid19.studyhub.nfdi4health.de/resource/{id}" in "Associated Studyhub Resource" section for a single study or assay view

--- app/helpers/studyhub_resources_helper.rb
+++ app/helpers/studyhub_resources_helper.rb
@@ -1,16 +1,28 @@
 module StudyhubResourcesHelper
-  
-  def persistent_studyhub_resource_id_json(resource)
-    label = "studyhub_resource.id"
-    pp resource.resource_type
+
+  def associated_studyhub_resource_web_link(resource)
+
     label = "studyhub_resource.#{resource.resource_type}"
 
-    url = URI.join(Seek::Config.site_base_host + '/', "#{resource.class.name.tableize}/", resource.id.to_s).to_s+".json"
+    #todo change hardcoded url
+    url = URI.join("http://covid19.studyhub.nfdi4health.de/resource/", resource.id.to_s).to_s
     content_tag :p, class: :id do
       content_tag(:strong) do
-        "Associated "+ t(label) + ':'
+        t(label) + ':'
       end + ' ' + link_to(url, url)
     end
   end
+
+  def studyhub_resource_in_seek_id_json(resource)
+
+    label = "studyhub_resource.id"
+    url = URI.join(Seek::Config.site_base_host + '/', "#{resource.class.name.tableize}/", resource.id.to_s).to_s+".json"
+    content_tag :p, class: :id do
+      content_tag(:strong) do
+         t(label) + ':'
+      end + ' ' + link_to(url, url)
+    end
+  end
+
 
 end
