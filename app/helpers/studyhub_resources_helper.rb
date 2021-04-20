cb4d49f33fe5c3ceeaa018bbcd91e4e4b89e4a60
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-04-20 14:12:35 +0200

    add associated studyhub resources section to a single study and assay view

--- app/helpers/studyhub_resources_helper.rb
+++ app/helpers/studyhub_resources_helper.rb
@@ -1,17 +1,14 @@
-
 module StudyhubResourcesHelper
+  
+  def persistent_studyhub_resource_id_json(resource)
+    label = "studyhub_resource.id"
+    pp resource.resource_type
+    label = "studyhub_resource.#{resource.resource_type}"
 
-
-  #@todo for testing reason, remove it later.
-  def persistent_resource_id_json(resource)
-    # FIXME: this contains some duplication of Seek::Rdf::RdfGeneration#rdf_resource - however not every model includes that Module at this time.
-    # ... its also a bit messy handling the version
     url = URI.join(Seek::Config.site_base_host + '/', "#{resource.class.name.tableize}/", resource.id.to_s).to_s+".json"
-
-
     content_tag :p, class: :id do
       content_tag(:strong) do
-        t('seek_id') + ':'
+        "Associated "+ t(label) + ':'
       end + ' ' + link_to(url, url)
     end
   end
