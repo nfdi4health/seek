a52deb87ebdf93a2a626451ced26daeca6788c54
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-05-31 12:35:32 +0200

    create studyhub resource type seed and combine with studyhub resource custom metadata seed; in the future there will be a seed file for nfdi4health related database pre population.

--- db/seeds/studyhub_resource.seeds.rb
+++ db/seeds/studyhub_resource.seeds.rb
@@ -1,3 +1,8 @@
+puts "Seeded Studyhub Resource Types"
+ActiveRecord::FixtureSet.create_fixtures(File.join(Rails.root, "config/default_data"), "studyhub_resource_types")
+
+
+puts "Seeded NFDI4Health Studyhub Resource Metadata"
 int_type = SampleAttributeType.find_or_initialize_by(title:'Integer')
 int_type.update_attributes(base_type: Seek::Samples::BaseType::INTEGER, placeholder: '1')
 
@@ -10,22 +15,21 @@ string_type.update_attributes(base_type: Seek::Samples::BaseType::STRING)
 cv_type = SampleAttributeType.find_or_initialize_by(title:'Controlled Vocabulary')
 cv_type.update_attributes(base_type: Seek::Samples::BaseType::CV)
 
-disable_authorization_checks do
+attributes = []
+StudyhubResourceType.all.each do |type|
+  attributes << { label: type.title }
+end
 
+disable_authorization_checks do
   # Studyhub resource type controlled vocabs
   resource_type_cv = SampleControlledVocab.where(title: 'NFDI4Health Resource Type').first_or_create!(
-    sample_controlled_vocab_terms_attributes: [{ label: 'Study' }, { label: 'Substudy' }, { label: 'Instrument' },
-                                               { label: 'Document' }]
+    sample_controlled_vocab_terms_attributes: attributes
   )
 
   # NFDI4Health Studyhub Study Metadata (resource_type: study and substudy)
   CustomMetadataType.where(title: 'NFDI4Health Studyhub Resource Metadata', supported_type: 'Study').first_or_create!(
     title: 'NFDI4Health Studyhub Resource Metadata', supported_type: 'Study',
     custom_metadata_attributes: [
-
-      # CustomMetadataAttribute.where(title: 'resource_id').create!(
-      #   title: 'resource_id', required: true, sample_attribute_type: int_type
-      # ),
 
       CustomMetadataAttribute.where(title: 'resource_web_studyhub').create!(
         title: 'resource_web_studyhub', required: false, sample_attribute_type: string_type
@@ -79,10 +83,6 @@ disable_authorization_checks do
   CustomMetadataType.where(title: 'NFDI4Health Studyhub Resource Metadata',supported_type: 'Assay').first_or_create!(
     title: 'NFDI4Health Studyhub Resource Metadata', supported_type: 'Assay',
     custom_metadata_attributes: [
-
-      # CustomMetadataAttribute.where(title: 'resource_id').create!(
-      #   title: 'resource_id', required: true, sample_attribute_type: int_type
-      # ),
 
       CustomMetadataAttribute.where(title: 'resource_web_studyhub').create!(
         title: 'resource_web_studyhub', required: false, sample_attribute_type: string_type
