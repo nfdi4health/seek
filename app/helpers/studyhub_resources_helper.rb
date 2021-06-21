b713344f5f7d6c040220aecd74a83f22e2c5a5a0
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-06-21 14:24:28 +0200

    customize the "CM" feature, in order to be able to use it in "studyhub resource" UI

--- app/helpers/studyhub_resources_helper.rb
+++ app/helpers/studyhub_resources_helper.rb
@@ -24,4 +24,39 @@ module StudyhubResourcesHelper
       end + ' ' + link_to(url, url)
     end
   end
+
+  def studyhub_custom_metadata_form_field_for_attribute(attribute, resource)
+
+    base_type = attribute.sample_attribute_type.base_type
+    clz = "custom_metadata_attribute_#{base_type.downcase}"
+    element_name = "studyhub_resource[custom_metadata_attributes][data][#{attribute.title}]"
+
+    value = resource[attribute.title]
+
+    placeholder = "e.g. #{attribute.sample_attribute_type.placeholder}" unless attribute.sample_attribute_type.placeholder.blank?
+
+    case base_type
+    when Seek::Samples::BaseType::TEXT
+      text_area_tag element_name, value, class: "form-control #{clz}"
+    when Seek::Samples::BaseType::DATE_TIME
+      content_tag :div, style:'position:relative' do
+        text_field_tag element_name,value, data: { calendar: 'mixed' }, class: "calendar form-control #{clz}", placeholder: placeholder
+      end
+    when Seek::Samples::BaseType::DATE
+      content_tag :div, style:'position:relative' do
+        text_field_tag element_name, value, data: { calendar: true }, class: "calendar form-control #{clz}", placeholder: placeholder
+      end
+    when Seek::Samples::BaseType::BOOLEAN
+      check_box_tag element_name, value, class: clz.to_s
+    when Seek::Samples::BaseType::SEEK_DATA_FILE
+      options = options_from_collection_for_select(DataFile.authorized_for(:view), :id,
+                                                   :title, value.try(:[],'id'))
+      select_tag(element_name, options, include_blank: !attribute.required?, class: "form-control #{clz}")
+    when Seek::Samples::BaseType::CV
+      controlled_vocab_form_field attribute, element_name, value
+    else
+      text_field_tag element_name, value, class: "form-control #{clz}", placeholder: placeholder
+    end
+  end
+
 end
