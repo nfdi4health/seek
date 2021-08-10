966168d8dcf127d3542e6ba9ea4ed68f85d5f985
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-08-10 22:38:26 +0200

    update StudyhubResourceSerializer according to new metadata schema

--- app/serializers/studyhub_resource_serializer.rb
+++ app/serializers/studyhub_resource_serializer.rb
@@ -4,15 +4,9 @@ class StudyhubResourceSerializer < SkeletonSerializer
       object.studyhub_resource_type.try(:key)
   end
 
-  attributes \
-  :nfdi_person_in_charge, :contact_stage, :data_source, \
-  :comment, :exclusion_mica_reason, :exclusion_seek_reason, :exclusion_studyhub_reason, \
-  :inclusion_studyhub, :inclusion_seek, :inclusion_mica, :resource_json
+  attributes :resource_json
 
   has_many :projects
-  has_one :study
-  has_one :assay
-  has_many :children
-  has_many :parents
   has_many :documents
+
 end
