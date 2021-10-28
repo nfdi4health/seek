c5a4ba1f6f0be25f86e5f17f1be618b96b5c137e
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-10-28 22:42:16 +0200

    remove "other" type for study

--- app/models/studyhub_resource_type.rb
+++ app/models/studyhub_resource_type.rb
@@ -3,7 +3,7 @@ class StudyhubResourceType < ApplicationRecord
   has_many :studyhub_resources, inverse_of: :studyhub_resource_type
 
   def is_studytype?
-    self.key == 'study' || self.key == 'substudy' || self.key == 'other_study'
+    self.key == 'study' || self.key == 'substudy'
   end
 
 end
