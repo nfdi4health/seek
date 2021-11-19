44e0ed5f3448a3e8abffddfe28c0d4538b07c50d
Xiaoming Hu <xiaoming.hu@h-its.org>
2021-11-19 12:03:52 +0100

    create a collapsible description for the attributes. I have joined the "short description" and the "additional description" with a <lb> tag. In UI only show the short description, only show the additional information when clicking "read more".

--- app/helpers/custom_metadata_helper.rb
+++ app/helpers/custom_metadata_helper.rb
@@ -10,8 +10,12 @@ module CustomMetadataHelper
 end
 
 def custom_metadata_attribute_description(description)
-  html = '<p class="help-block">'
-  html += '<small>'+description+'</small>'
-  html += '</p>'
+  html = '<div class="addtional-info help-block">'
+  html += '<small>' + description.split("<lb>").first + '<a href="#"> read more</a>'
+  html += '</small>'
+  html += '<div class="less">'
+  html += '<small>'+description.split("<lb>").last+'</small>'
+  html += '</div>'
+  html += '</div>'
   html.html_safe
 end
