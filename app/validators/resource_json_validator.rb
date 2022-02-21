4ce93b6f300f8b8b78c7671d404112e63575572c
Xiaoming Hu <xiaoming.hu@h-its.org>
2022-02-21 13:22:44 +0100

    separate the json_schema for "the general resource" and "the study design"

--- app/validators/resource_json_validator.rb
+++ app/validators/resource_json_validator.rb
@@ -2,7 +2,8 @@ require 'json-schema'
 
 class ResourceJsonValidator < ActiveModel::EachValidator
 
-  JSONAPI_SCHEMA_FILE_PATH = File.join(Rails.root, 'public', 'api', 'jsonapi-schema-resource-json.json')
+  JSONAPI_SCHEMA_FILE_PATH = File.join(Rails.root, 'public', 'api', 'jsonapi-schema-resource-json-resource.json')
+  JSONAPI_SCHEMA_STUDY_DESIGN_FILE_PATH = File.join(Rails.root, 'public', 'api', 'jsonapi-schema-resource-json-addtional-study-design.json')
 
   def validate_each(record, attribute, value)
     #record=>#<StudyhubResource id: nil, resource_json: {"resource_titles"=>[{"title"=>"National Pandemi....
@@ -12,6 +13,7 @@ class ResourceJsonValidator < ActiveModel::EachValidator
     Rails.logger.info('Validator:starting to check resource_json............')
 
     begin
+      Rails.logger.info('Validator:starting to check resource............')
       JSON::Validator.validate!(JSONAPI_SCHEMA_FILE_PATH, value)
     rescue JSON::Schema::ValidationError => e
       Rails.logger.info("+++++++++++++++++++++++++")
@@ -19,6 +21,16 @@ class ResourceJsonValidator < ActiveModel::EachValidator
       record.errors.add(attribute.to_sym, e.message)
     end
 
+    if record.is_studytype?
+      begin
+        Rails.logger.info('Validator:starting to check study design............')
+        JSON::Validator.validate!(JSONAPI_SCHEMA_STUDY_DESIGN_FILE_PATH, value)
+      rescue JSON::Schema::ValidationError => e
+        Rails.logger.info("+++++++++++++++++++++++++")
+        Rails.logger.info("e:"+e.message)
+        record.errors.add(attribute.to_sym, e.message)
+      end
+    end
   end
 
 end
