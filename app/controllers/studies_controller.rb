3b2b9715db3ebeb13737c972a11876de0efd69e5
Alexander Strübing <alexander.struebing@gmail.com>
2022-09-20 12:13:42 +0200

    Use Success Banner instead of Dedicated Page

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -349,6 +349,11 @@ class StudiesController < ApplicationController
     url = Seek::Config.n4h_url.blank? ? nil : Seek::Config.n4h_url
     endpoint = Nfdi4Health::Client.new(url)
     endpoint.publish_study(@study.to_json)
+
+    respond_to do |format|
+      flash[:notice] = "#{t('study')} was successfully published."
+      format.html { redirect_to(@study) }
+    end
   end
 
   private
